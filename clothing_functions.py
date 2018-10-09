49b17125e3362aaa893daeb4fc9ba9f9668285f6
hanzh0420 <hanzh0420@ufl.edu>
2018-10-09 14:15:26 -0400

    modified
    modified

--- clothing_functions.py
+++ clothing_functions.py
@@ -2,18 +2,46 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Sep 21 10:47:34 2018
-
+Functions used in Women’s Clothing E-Commerce Projects
 @author: hanzhang
 """
 # functions for the women E-Commerce review sentiment analysis 
 import seaborn as sns
-import matplotlib.pyplot as plt
 import nltk 
 import string
 
-def plot_dist(data,column,ax):
-    sns.distplot(data[column], color="b", label="Skewness : %.2f  Kurtosis : %.2f"\
-                %(data[column].skew(),data[column].kurt()),bins=25, ax=ax,kde=False)
+from wordcloud import WordCloud
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+def plot_cloud(words_dist,size):
+    """
+    Plot the wordcloud figure
+    """
+    mpl.rcParams['figure.figsize'] = (10.0, 10.0)
+    mpl.rcParams['font.size'] = 12
+    mpl.rcParams['savefig.dpi'] = 300
+    mpl.rcParams['figure.subplot.bottom'] = .1
+    
+    wordcloud = WordCloud(width=1600, height=800,
+                          background_color='black').generate_from_frequencies(words_dist)
+    
+    fig = plt.figure(figsize=size, facecolor='k')
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    #plt.title(title, fontsize=50, color='y')
+    plt.tight_layout()
+   # plt.savefig('{}.png'.format(title), format='png', dpi=300)
+    plt.show()
+
+def plot_dist(data,column,ax=None):
+    if ax is None: 
+        sns.distplot(data[column], color="b", label="Skewness : %.2f  Kurtosis : %.2f"\
+                %(data[column].skew(),data[column].kurt()),bins=25, kde=False)    
+    else:
+        sns.distplot(data[column], color="b", label="Skewness : %.2f  Kurtosis : %.2f"\
+                %(data[column].skew(),data[column].kurt()),bins=25, ax=ax, kde=False)  
+        
     ax.legend(loc="best")
     return None
 
@@ -44,6 +72,7 @@ stopwords_en = stopwords.words('english')
 #from nltk.stem import SnowballStemmer,WordNetLemmatizer
 #stemmer=SnowballStemmer('english')
 #lemma=WordNetLemmatizer() # worked the same way as tonized 
+
 def text_process(mess):
     """
     Takes in a string of text, then performs the following:
@@ -55,12 +84,19 @@ def text_process(mess):
                if char not in string.punctuation and not char.isdigit()])
     nopunc = ''.join(nopunc)
     new_word=[((word)) for word in nopunc.split() if word not in stopwords_en]
-    #new_word=[stemmer.stem(word) for word in new_word.split()]
+        #new_word=[stemmer.stem(word) for word in new_word.split()]
     return new_word 
 
+class clothing_text:
+    def __init__(self,s):
+        self.s=s
+       # self.s = s
 
-def word_freq(s):
-    txt = s.str.lower().str.replace(r'\W+', ' ').str.cat(sep=' ')  
-    words = text_process(txt)
-    words_dist = nltk.FreqDist(w for w in words) 
-    return words_dist
+    def word_freq(self):
+        txt = self.s.str.lower().str.replace(r'\W+', ' ').str.cat(sep=' ')  
+        words = text_process(txt)
+        words_dist = nltk.FreqDist(w for w in words) 
+        return words_dist
+   
+
+
